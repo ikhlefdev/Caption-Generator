e90b4ed738d89bda9f8f3b050aedc2137c667811
ikhlef meriem <m.ikhlef@esi-sba.dz>
2026-08-03 19:01:08 +0100

    added the ai automation logic

--- captions/logic.py
+++ captions/logic.py
@@ -1,3 +1,4 @@
+
 def generate_caption(product, platform, tone, feature, cta_type):
     hashtag = f"#{product.replace(' ', '')} #{platform.replace(' ', '')} #{feature.replace(' ', '')}"
     match tone:
@@ -9,3 +10,25 @@ def generate_caption(product, platform, tone, feature, cta_type):
             return f"Introducing the {product} on {platform}. This {feature} {product} is designed for professionals. {cta_type} to learn more. {hashtag}"
         case _:
             return "Invalid tone selected"
+
+
+
+from dotenv import load_dotenv
+import os
+from google import genai
+
+os.environ["HTTP_PROXY"] = "http://192.168.49.1:8282"   
+os.environ["HTTPS_PROXY"] = "http://192.168.49.1:8282"
+def generate_ai_caption(product, platform, tone, feature, cta_type):
+# Load environment variables from .env file
+   load_dotenv()
+   api_key = os.getenv("GEMINI_API_KEY")
+   client = genai.Client(api_key=api_key)
+   response =client.models.generate_content(
+      
+      model="gemini-3.6-flash",
+      contents=f"Generate a caption for a {product} on {platform} with a {tone} tone, highlighting the {feature} feature, and including a {cta_type} call-to-action and generate relevant hashtags.",
+    )
+
+   return response.text
+
--- captions/views.py
+++ captions/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from captions.logic import generate_caption
+from captions.logic import generate_caption, generate_ai_caption
 
 def home(request):
     generated_caption = None
+    generated_ai_caption = None
     if request.method == 'POST':
         product = request.POST.get('product_name')
         platform = request.POST.get('platform')
@@ -11,6 +12,11 @@ def home(request):
         feature = request.POST.get('feature')
         cta = request.POST.get('cta_type')
 
+
+
         # Generate caption based on the input values
         generated_caption = generate_caption(product, platform, tone, feature, cta)
-    return render(request, 'captions/home.html', {'generated_caption': generated_caption})
+        generated_ai_caption = generate_ai_caption(product, platform, tone, feature, cta)
+
+
+    return render(request, 'captions/home.html', {'generated_caption': generated_caption, 'generated_ai_caption': generated_ai_caption})
